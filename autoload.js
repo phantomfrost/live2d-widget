70e3c19ec8109bfbecf0faf7776e52cbd6eeabf9
phantomfrost <78469955+phantomfrost@users.noreply.github.com>
2023-05-13 22:56:50 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,6 +1,6 @@
 // live2d_path 参数建议使用绝对路径
 //const live2d_path = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
-const live2d_path = "/";
+const live2d_path = "https://live2d-xuekwan.netlify.app/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
